d0f36c52815e64314759befd05f21d0ce8825ff6
Gracie <m.graciemcguire@gmail.com>
2021-11-03 12:33:45 -0400

    adds solution

--- lib/binary_search_tree.rb
+++ lib/binary_search_tree.rb
@@ -7,12 +7,51 @@ class BinarySearchTree
     @root = root
   end
 
-  def search(value)
-    # your code here
-  end
-
   def insert(value)
-    # your code here
+    new_node = Node.new(value)
+    if @root == nil
+      @root = new_node
+      return self
+    end
+
+    current = @root
+
+    while(true)
+      return nil if value == current.value
+      if value < current.value 
+        if current.left == nil
+          current.left = new_node 
+          return self
+        end
+        current = current.left
+      else
+        if current.right == nil
+          current.right = new_node 
+          return self
+        end
+        current = current.right
+      end
+    end
   end
 
+  def search(value)
+    return nil if @root == nil
+
+    current = @root
+    found = false
+
+    while current && !found
+      if value < current.value
+        current = current.left
+      elsif value > current.value
+        current = current.right
+      else
+        found = true
+      end
+    end
+    found ? current : nil
+  end
 end 
+
+
+
--- lib/node.rb
+++ lib/node.rb
@@ -1,6 +1,5 @@
 class Node
   attr_accessor :value, :left, :right
-
   def initialize(value, left = nil, right = nil)
     @value = value
     @left = left
